ff05450bc5e18ef8d8e5b03ef55d9618092d1505
Sarthak <sarthaktomar2143@gmail.com>
2022-11-06 21:18:38 +0530

    Polish solutions.

--- src/01.py
+++ src/01.py
@@ -1,11 +1,5 @@
 # Problem 1: Multiples of 3 and 5
 
-"""
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-
-Find the sum of all the multiples of 3 or 5 below 1000.
-"""
-
 def multiples(num, limit):
     res = []
     for i in range(num, limit):
@@ -13,8 +7,12 @@ def multiples(num, limit):
             res.append(i)
     return res
 
-mul_3 = multiples(3, 1000)
-mul_5 = multiples(5, 1000)
-res = set(mul_3 + mul_5)
+def main():
+    mul_3 = multiples(3, 1000)
+    mul_5 = multiples(5, 1000)
+    res = set(mul_3 + mul_5)
 
-print(f"Sum: {sum(res)}") # => 233168
+    print(f"Sum: {sum(res)}") # => 233168
+
+if __name__ == "__main__":
+    main()
--- src/03.py
+++ src/03.py
@@ -1,21 +1,5 @@
 # Problem 3: Largest Prime Factor
 
-"""
-The prime factors of 13195 are 5, 7, 13 and 29.
-
-What is the largest prime factor of the number 600851475143 ?
-"""
-
-import math
-
-def isPrime(n):
-    if n == 1 or n == 2: return True
-    c = int(math.sqrt(n))
-    for i in range(2, c+1):
-        if n % i == 0:
-            return False
-    return True
-
 def send_factors(n):
     res = []
     while n != 1 :
@@ -27,4 +11,8 @@ def send_factors(n):
                 break
     return list(set(res))
 
-print(max(send_factors(600851475143))) # => 6857
+def main():
+    print(max(send_factors(600851475143))) # => 6857
+
+if __name__ == "__main__":
+    main()
